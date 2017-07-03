7f0f05c2a44d8a4c761fdaec5d10464b4a25921e
R.Nazeri <reza.armani75@gmail.com>
2017-07-03 15:53:43 +0430

    create splash

---
+++ app/src/main/java/ir/co/ts/firstproject/BaseActivity.java
@@ -0,0 +1,10 @@
+package ir.co.ts.firstproject;
+
+import android.support.v7.app.AppCompatActivity;
+
+/***
+ * Created by reza on 7/3/17.
+ ***/
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/ir/co/ts/firstproject/SplashActivity.java
@@ -0,0 +1,23 @@
+package ir.co.ts.firstproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3 * 1000);
+    }
+}
